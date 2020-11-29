7e8f2dd51a80d3639a860f601757927737f1fcd8
alexandergm <sasha_261@outlook.com>
2020-11-30 00:07:37 +0300

    UI + Skybox

---
+++ Assets/AlexGM.Trash/SetScore.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetScore : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private Text _score;
+
+    public void InstantiateScore(int point)
+    {
+        _score.text = point.ToString();
+    }
+}
